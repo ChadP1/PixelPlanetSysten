866a9084f3ce75e61afc8bc6bb459484cd9a7a59
chad parr <P1@chads-iMac.local>
2016-02-05 15:46:20 -0600

    class to handle data, began to amke action handlers

--- SolarSystemPixel/ViewController.swift
+++ SolarSystemPixel/ViewController.swift
@@ -9,7 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    enum Solar: String {
+        
+        case Sun
+        case Venus
+        case Mercury
+        case Jupiter
+        case Saturn
+        case Earth
+        case Mars
+        case Neptune
+        case Uranus
+        
+    }
+    
+    var planet: planetClass! 
+    var currentPlanet: Solar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +37,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
 
+   func processPlanet(btn: UIButton!) {
+    
+    if  currentPlanet == Solar.Earth {
+        
+    } else if currentPlanet == Solar.Sun {
+        
+    } else if currentPlanet == Solar.Mars {
+        
+    } else if currentPlanet == Solar.Mercury {
+        
+    } else if currentPlanet == Solar.Neptune {
+        
+    } else if currentPlanet == Solar.Jupiter  {
+        
+    } else if currentPlanet == Solar.Venus {
+        
+    } else if currentPlanet == Solar.Uranus {
+        
+    } else if currentPlanet == Solar.Saturn {
+        
+        
+        performSegueWithIdentifier("planetInfo", sender: planet)
+        
+    }
+    
+    
+    
+    
+    
 }
 
+}
---
+++ SolarSystemPixel/planetClass.swift
@@ -0,0 +1,31 @@
+//
+//  planetClass.swift
+//  SolarSystemPixel
+//
+//  Created by chad parr on 2/5/16.
+//  Copyright © 2016 chad parr. All rights reserved.
+//
+
+import Foundation
+
+class planetClass{
+    
+    private var _type: String!
+    private var _name: String!
+    private var _size: String!
+    private var _distance: String!
+    
+    var type: String {
+        return _type
+    }
+    var name: String {
+        return _name
+    }
+    var size: String {
+        return _size
+    }
+    var distance: String {
+        return _distance
+    }
+    
+}
